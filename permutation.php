5c804e57d8af2e721c3c9d90cf886f56a2c9ddb8
ceooten <sdtrojans55@gmail.com>
2017-09-21 19:37:45 -0400

    Add files via upload

--- permutation.php
+++ permutation.php
@@ -4,38 +4,53 @@ fclose($writeFile);
 echo '<h1>Word Game</h1>';
 $myfile = fopen("letters.txt", "r");
 
-$word = fread($myfile, filesize("letters.txt"));
-echo 'Your letters are: ' . $word;
+$word2 = fread($myfile, filesize("letters.txt"));
+echo 'Your letters are: ' . $word2;
+echo '<br><br><button onclick="history.go(-1);">Back</button>';
 fclose($myfile);
 echo '<br>';
 echo "All permutations: <br>";
 
-function permutations($word) {
-    $result = array();
-    if (strlen($word) <= 1) {
-        $result[] = $word;
-        return $result;
-    } else {
-        for ($i = 0; $i < strlen($word); $i++) {
-            $shorter = substr($word, 0, $i) . substr($word, $i + 1);
-            $shorterPerms = array();
-            $shorterPerms[] = permutations($shorter);
+function permArray($word2)  
+		{ 
+			$result2 = array(); 
+			if(strlen($word2) <= 1)
+			{
+				$result2[] = $word2; 
+				$writeFile2 = fopen("perms.txt", "w") or die('Cannot open file: '.$writeFile);
+				for($o = 0; $o < count($result2); $o++)
+				{		
+					fwrite($writeFile2, $result2[$o]."\r\n"); 
+				} 
+				fclose($writeFile2);
+				return $result2; 
+			}
+			else
+			{
+				for($i = 0; $i < strlen($word2); $i++)
+				{
+					$shorter2 = substr($word2, 0, $i).substr($word2, $i+1);
+					$shorterPerms2 = array();
+					$shorterPerms2[] = permArray($shorter2); 
+					
+					foreach($shorterPerms2 as $value2)
+					{	
+						for($j = 0; $j < count($value2);$j++)
+						{
+							$result2[] = $word2[$i] . $value2[$j];
+						} 
+					}
+					unset($value2); 
+				}
+				$writeFile2 = fopen("perms.txt", "w") or die('Cannot open file: '.$writeFile);
+				for($o = 0; $o < count($result2); $o++)
+				{		
+					fwrite($writeFile2, $result2[$o]."\r\n"); 
+				} 
+				fclose($writeFile2);
+				return $result2;  
+			}
+		}
 
-            foreach ($shorterPerms as $value) {
-                for ($j = 0; $j < count($value); $j++) {
-                    $result[] = $word[$i] . $value[$j];
-                }
-            }
-            unset($value);
-        }
-        //write the results array to a file each time the function is called
-        $writeFile = fopen("perms.txt", "w") or die('Cannot open file: ' . $writeFile);
-        for ($o = 0; $o < count($result); $o++) {
-            fwrite($writeFile, $result[$o] . "\r\n");
-        }
-        fclose($writeFile);
-        return $result;
-    }
-}
-echo nl2br(file_get_contents( "perms.txt" ));
-permutations($word);
+permArray($word2);
+echo nl2br(file_get_contents("perms.txt"));
--- words.php
+++ words.php
@@ -1,9 +1,10 @@
 <?php
+
 $clearfile2 = fopen("words2.txt", "w");
 fclose($clearfile2);
 $clearfile = fopen("words.txt", "w");
 fclose($clearfile);
-$wordfile = fopen("words.txt", "a");
+
 $FinalWordArray = array();
 $lines = file("perms.txt", FILE_IGNORE_NEW_LINES);
 $lineCount = 0;
@@ -18,39 +19,97 @@ function get_all_substrings($input) {
     }
     return $subs;
 }
-//CHANGES----------------------------------------------
+//Generate permutations from the random letters
+function initialPermArray($word3)  
+		{ 
+			$result2 = array(); 
+			if(strlen($word3) <= 1)
+			{
+				$result2[] = $word3; 
+				$writeFile2 = fopen("perms.txt", "w") or die('Cannot open file: '.$writeFile);
+				for($o = 0; $o < count($result2); $o++)
+				{		
+					fwrite($writeFile2, $result2[$o]."\r\n"); 
+				} 
+				fclose($writeFile2);
+				return $result2; 
+			}
+			else
+			{
+				for($i = 0; $i < strlen($word3); $i++)
+				{
+					$shorter2 = substr($word3, 0, $i).substr($word3, $i+1);
+					$shorterPerms2 = array();
+					$shorterPerms2[] = permArray($shorter2); 
+					
+					foreach($shorterPerms2 as $value2)
+					{	
+						for($j = 0; $j < count($value2);$j++)
+						{
+							$result2[] = $word3[$i] . $value2[$j];
+						} 
+					}
+					unset($value2); 
+				}
+				$writeFile2 = fopen("perms.txt", "w") or die('Cannot open file: '.$writeFile);
+				for($o = 0; $o < count($result2); $o++)
+				{		
+					fwrite($writeFile2, $result2[$o]."\r\n"); 
+				} 
+				fclose($writeFile2);
+				return $result2;  
+			}
+		}
 
-function permutations($word) {
-    $result = array();
-    if (strlen($word) <= 1) {
-        $result[] = $word;
-        return $result;
-    } else {
-        for ($i = 0; $i < strlen($word); $i++) {
-            $shorter = substr($word, 0, $i) . substr($word, $i + 1);
-            $shorterPerms = array();
-            $shorterPerms[] = permutations($shorter);
 
-            foreach ($shorterPerms as $value) {
-                for ($j = 0; $j < count($value); $j++) {
-                    $result[] = $word[$i] . $value[$j];
-                }
-            }
-            unset($value);
-        }
-        //write the results array to a file each time the function is called
-        $writeFile = fopen("words2.txt", "a") or die('Cannot open file: ' . $writeFile);
-        for ($o = 0; $o < count($result); $o++) {
-            fwrite($writeFile, $result[$o] . "\r\n");
-        }
-        fclose($writeFile);
-        return $result;
-    }
-}
-
-//CHANGES----------------------------------------------
+$myfile = fopen("letters.txt", "r");
+$word3 = fread($myfile, filesize("letters.txt"));
+fclose($myfile);
+InitialPermArray($word3);
 
 
+function permArray($word2)  
+		{ 
+			$result2 = array(); 
+			if(strlen($word2) <= 1)
+			{
+				$result2[] = $word2; 
+				$writeFile2 = fopen("words2.txt", "a") or die('Cannot open file: '.$writeFile);
+				for($o = 0; $o < count($result2); $o++)
+				{		
+					fwrite($writeFile2, $result2[$o]."\r\n"); 
+				} 
+				fclose($writeFile2);
+				return $result2; 
+			}
+			else
+			{
+				for($i = 0; $i < strlen($word2); $i++)
+				{
+					$shorter2 = substr($word2, 0, $i).substr($word2, $i+1);
+					$shorterPerms2 = array();
+					$shorterPerms2[] = permArray($shorter2); 
+					
+					foreach($shorterPerms2 as $value2)
+					{	
+						for($j = 0; $j < count($value2);$j++)
+						{
+							$result2[] = $word2[$i] . $value2[$j];
+						} 
+					}
+					unset($value2); 
+				}
+				$writeFile2 = fopen("words2.txt", "a") or die('Cannot open file: '.$writeFile);
+				for($o = 0; $o < count($result2); $o++)
+				{		
+					fwrite($writeFile2, $result2[$o]."\r\n"); 
+				} 
+				fclose($writeFile2);
+				return $result2;  
+			}
+		}
+//Generate substrings              
+$wordfile = fopen("words.txt", "a");
 for ($i = 0; $i < count($lines); $i++) {
 
     $string = $lines[$i];
@@ -65,12 +124,13 @@ fclose($wordfile);
 //array containing every substring 
 $lines2 = file("words.txt", FILE_IGNORE_NEW_LINES);
 //takes each substring and finds the permutation of it
-for ($i = 0; $i < $lineCount; $i++) {$lines2[$i];
-    $word = $lines2[$i];
-    permutations($word);
+for ($i = 0; $i < $lineCount; $i++) {
+    $lines2[$i];
+    $word2 = $lines2[$i];
+    permArray($word2);
 }
-//fclose($wordfile2);
 
+//read in files and store them as an array
 $lines3 = file("words2.txt", FILE_IGNORE_NEW_LINES);
 $uniquelines3 = array_values(array_unique($lines3));
 $lines4 = file("engmix.txt", FILE_IGNORE_NEW_LINES);
@@ -79,6 +139,7 @@ $lines4 = array_map('strtoupper', $lines4);
 
 echo '<table><th style="vertical-align: top; padding-right: 200px;">';
 echo 'All possible combinations: <br>';
+
 //PRINTS OUT ALL POSSIBLE UNIQUE COMBINATIONS OF THE RANDOM LETTERS
 for ($i = 0; $i < count($uniquelines3); $i++) {
     echo $uniquelines3[$i] . "<br>";
@@ -86,6 +147,7 @@ for ($i = 0; $i < count($uniquelines3); $i++) {
 
 echo '</th><th style="vertical-align: top;">';
 echo 'Found words: <br>';
+
 //FINDS ALL VALUES IN UNIQUELINES3 THAT ARE LOCATED IN THE DICTIONARY FILE
 for ($i = 0; $i < count($uniquelines3); $i++) {
 
